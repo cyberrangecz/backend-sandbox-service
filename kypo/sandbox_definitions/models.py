b5db8f6078cc121516cc6b40571d2b8ac10f622c
Miloslav Staněk <445225@mail.muni.cz>
2019-11-04 16:54:17 +0100

    add definition models and serializers

--- kypo/sandbox_definitions/models.py
+++ kypo/sandbox_definitions/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Definition(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.TextField()
+    rev = models.TextField(default='master')
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return "ID: {0.id}, NAME: {0.name}, URL: {0.url}, REV: {0.rev}".format(self)
