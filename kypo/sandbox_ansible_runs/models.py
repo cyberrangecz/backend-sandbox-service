e86d7a9a2aa26de852e00bdff45d3339413a5d4c
Miloslav Staněk <milostanek@gmail.com>
2019-11-05 16:07:50 +0100

    implementent new model

--- kypo/sandbox_ansible_runs/models.py
+++ kypo/sandbox_ansible_runs/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from ..sandbox_instances.models import ExternalDependency, Stage
+
+
+class AnsibleAllocationStage(Stage):
+    repo_url = models.TextField()
+    rev = models.TextField()
+
+    def __str__(self):
+
+        return super().__str__() + \
+               ", REPO_URL: {0.repo_url}, REV: {0.rev}".format(self)
+
+
+class AnsibleOutput(models.Model):
+    stage = models.ForeignKey(
+        AnsibleAllocationStage,
+        on_delete=models.CASCADE,
+        related_name='outputs'
+    )
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return "ID: {0.id}, STAGE: {0.stage.id}, CONTENT: {0.content}".format(self)
+
+
+class DockerContainer(ExternalDependency):
+    container_id = models.TextField()
--- kypo/sandbox_instances/models.py
+++ kypo/sandbox_instances/models.py
@@ -1,3 +1,166 @@
 from django.db import models
+from django.utils import timezone
+from model_utils.managers import InheritanceManager
 
-# Create your models here.
+from ..sandbox_definitions.models import Definition
+
+
+class Pool(models.Model):
+    definition = models.ForeignKey(
+        Definition,
+        on_delete=models.PROTECT
+    )
+    max_size = models.IntegerField()
+    private_management_key = models.TextField()
+    public_management_key = models.TextField()
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return "ID: {0.id}, DEFINITION: {0.definition.id}, MAX_SIZE: {0.max_size}".format(self)
+
+    def get_keypair_name(self) -> str:
+        """Returns a name of the management key-pair for this pool."""
+        return self.definition.name + '-' + str(self.id)
+
+
+class SandboxAllocationUnit(models.Model):
+    pool = models.ForeignKey(
+        Pool,
+        on_delete=models.CASCADE,
+        related_name='allocation_units',
+    )
+
+    def get_stack_name(self) -> str:
+        """Returns a name of the stack for this sandbox"""
+        return self.pool.definition.name + '-' + str(self.id)
+
+
+class Lock(models.Model):
+    pass
+
+
+class Sandbox(models.Model):
+    allocation_unit = models.OneToOneField(
+        SandboxAllocationUnit,
+        on_delete=models.PROTECT,
+        related_name='sandbox',
+    )
+    lock = models.OneToOneField(
+        Lock,
+        on_delete=models.PROTECT,
+        related_name='sandbox',
+        null=True,
+        default=None,
+    )
+    private_user_key = models.TextField()
+    public_user_key = models.TextField()
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return "ID: {0.id}, ALLOCATION_UNIT: {0.allocation_unit.id}, " \
+               "LOCK: {0.lock.id}".format(self)
+
+    def get_stack_name(self) -> str:
+        """Returns a name of the stack for this sandbox"""
+        return self.allocation_unit.get_stack_name()
+
+
+class SandboxRequest(models.Model):
+    """Abstract base class for Sandbox Requests."""
+    allocation_unit = models.ForeignKey(
+        SandboxAllocationUnit,
+        on_delete=models.CASCADE,
+        related_name='%(class)ss',
+    )
+    created = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        abstract = True
+        ordering = ['created']
+
+    def __str__(self):
+        return "ID: {0.id}, ALLOCATION_UNIT: {0.allocation_unit.id}, CREATED: {0.created}"\
+            .format(self)
+
+    def get_stack_name(self) -> str:
+        """Returns a name of the stack for this sandbox"""
+        return self.allocation_unit.get_stack_name()
+
+
+class AllocationRequest(SandboxRequest):
+    pass
+
+
+class CleanupRequest(SandboxRequest):
+    pass
+
+
+class Stage(models.Model):
+    """Abstract base class for stages."""
+    start = models.DateTimeField(null=True, default=None)
+    end = models.DateTimeField(null=True, default=None)
+
+    failed = models.BooleanField(default=False)
+    error_message = models.TextField(null=True)
+
+    @property
+    def is_finished(self):
+        return self.end is not None
+
+    @property
+    def is_running(self):
+        return self.start is not None and self.end is None
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return "START: {0.start}, END: {0.end} FAILED: {0.failed}".format(self)
+
+    def mark_failed(self, exception=None):
+        self.failed = True
+        if exception:
+            self.error_message = str(exception)
+        self.save()
+
+
+class AllocationStage(Stage):
+    request = models.ForeignKey(
+        AllocationRequest,
+        on_delete=models.CASCADE,
+        related_name='stages',
+    )
+
+    objects = InheritanceManager()
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return "{0.id}, ".format(self) + super().__str__()
+
+
+class StackAllocationStage(AllocationStage):
+    status = models.CharField(null=True, max_length=30)
+    status_reason = models.TextField(null=True)
+
+    def __str__(self):
+        return super().__str__() + \
+               ", STATUS: {0.status}, STATUS_REASON: {0.status_reason}".format(self)
+
+
+class ExternalDependency(models.Model):
+    class Meta:
+        abstract = True
+
+
+class HeatStack(ExternalDependency):
+    pass
+
+
+class SystemProcesses(ExternalDependency):
+    pass
