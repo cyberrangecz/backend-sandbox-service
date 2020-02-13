77b8c87464fe4a852e8feacb47c0395ad9e86e66
Miloslav Staněk <445225@mail.muni.cz>
2020-02-13 15:08:01 +0100

    add definition tests

---
+++ kypo/sandbox_definition_app/tests/test_definition.py
@@ -0,0 +1,40 @@
+import pytest
+from rest_framework.exceptions import ValidationError
+
+from kypo.sandbox_definition_app.lib import definition_service
+from kypo.sandbox_definition_app.models import Definition
+
+pytestmark = pytest.mark.django_db
+
+
+class TestCreateDefinition:
+    url = 'def-url'
+    rev = 'def-rev'
+    name = 'def-name'
+
+    def test_create_definition_success(self, mocker):
+        mocker.patch("kypo.openstack_driver.ostack_client.KypoOstackClient"
+                     ".validate_sandbox_definition")
+        mock = mocker.Mock()
+        mock.configure_mock(name='def-name')
+        mocker.patch("kypo.sandbox_definition_app.lib.definition_service.get_definition",
+                     return_value=mock)
+
+        definition_service.create_definition(url=self.url, rev=self.rev)
+
+        # Definition name is tested by get
+        database_definition = Definition.objects.get(name=self.name)
+        assert database_definition.url == self.url
+        assert database_definition.rev == self.rev
+
+    def test_create_definition_nonunique(self, mocker):
+        mocker.patch("kypo.openstack_driver.ostack_client.KypoOstackClient"
+                     ".validate_sandbox_definition")
+        mock = mocker.Mock()
+        mock.configure_mock(name='def-name')
+        mocker.patch("kypo.sandbox_definition_app.lib.definition_service.get_definition",
+                     return_value=mock)
+
+        definition_service.create_definition(url=self.url, rev=self.rev)
+        with pytest.raises(ValidationError):
+            definition_service.create_definition(url=self.url, rev=self.rev)
--- kypo/sandbox_definition_app/tests/test_dummy.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-class TestDummy:
-
-    @pytest.mark.tryfirst
-    def test_always_true(self):
-        assert True
