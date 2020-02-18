2db19832bf5d118197d8786dd4cd5779079ce35c
Miloslav Staněk <445225@mail.muni.cz>
2020-02-18 14:11:49 +0100

    fix integration test

--- kypo/sandbox_service_project/tests/conftest.py
+++ kypo/sandbox_service_project/tests/conftest.py
@@ -8,7 +8,7 @@ INTEGRATION_TEST_TEMPLATE = "template.yml"
 
 
 def data_path_join(file: str, data_dir: str = TESTING_DATA_DIR) -> str:
-    return os.path.join(re.sub('\\.', '/', __package__), data_dir, file)
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), data_dir, file)
 
 
 @pytest.fixture
