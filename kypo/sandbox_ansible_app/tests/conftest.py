5046dc3731457297af5593da90c03e2d0650e24c
Miloslav Staněk <milostanek@gmail.com>
2020-04-03 19:06:57 +0200

    add ansible tests

--- kypo/sandbox_ansible_app/tests/conftest.py
+++ kypo/sandbox_ansible_app/tests/conftest.py
@@ -2,6 +2,8 @@ import pytest
 import os
 import jsonpickle
 import yaml
+from django.core.management import call_command
+
 from kypo.topology_definition.models import TopologyDefinition
 
 TESTING_DATA_DIR = 'assets'
@@ -9,10 +11,17 @@ TESTING_DATA_DIR = 'assets'
 TESTING_STACK = 'stack.json'
 TESTING_INVENTORY = 'inventory.yml'
 TESTING_DEFINITION = 'definition.yml'
+TESTING_DATABASE = 'database.yaml'
 
 
 def data_path_join(file: str, data_dir: str = TESTING_DATA_DIR) -> str:
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), data_dir, file)
+
+
+@pytest.fixture(scope='session')
+def django_db_setup(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        call_command('loaddata', data_path_join(TESTING_DATABASE))
 
 
 @pytest.fixture
---
+++ kypo/sandbox_ansible_app/tests/test_ansible.py
@@ -0,0 +1,24 @@
+import pytest
+
+from kypo.sandbox_ansible_app.lib.ansible import AnsibleDockerRunner
+from kypo.sandbox_instance_app.models import Sandbox
+
+pytestmark = pytest.mark.django_db
+
+
+class TestPrepareInventoryFile:
+    @pytest.fixture(autouse=True)
+    def set_up(self, mocker, stack):
+        self.client = mocker.patch('kypo.sandbox_common_lib.utils.get_ostack_client')
+        self.client.get_sandbox.return_value = stack
+        self.save_file = mocker.patch(
+            'kypo.sandbox_ansible_app.lib.ansible.AnsibleDockerRunner.save_file')
+        yield
+
+    def test_prepare_inventory_file_success(self, mocker, top_def):
+        mock_inventory = mocker.patch('kypo.sandbox_ansible_app.lib.ansible.Inventory')
+        dir_path = '/tmp'
+        sandbox = Sandbox.objects.get(pk=1)
+        AnsibleDockerRunner().prepare_inventory_file(dir_path, sandbox, top_def)
+
+        mock_inventory.assert_called_once()
