211679873cbb6f5800963645cfb92482c9b19a56
Miloslav Staněk <milostanek@gmail.com>
2020-04-22 23:49:56 +0200

    update topology

--- kypo/sandbox_instance_app/lib/topology.py
+++ kypo/sandbox_instance_app/lib/topology.py
@@ -1,10 +1,6 @@
 import structlog
-from kypo.openstack_driver.sandbox_topology import SandboxTopology as Stack,\
-    BR_NET_NAME, UAN_NET_NAME
 
-from django.conf import settings
-from kypo.sandbox_instance_app.models import Sandbox
-from kypo.sandbox_definition_app.lib import definitions
+from kypo.openstack_driver.topology_instance import TopologyInstance, UAN_NET_NAME, BR_NET_NAME
 
 LOG = structlog.getLogger()
 
@@ -24,23 +20,18 @@ class Topology:
             self.real_port = real_port
             self.dummy_port = dummy_port
 
-    def __init__(self, sandbox: Sandbox, stack: Stack):
-        self.hosts = []
-        self.routers = []
-        self.switches = []
+    def __init__(self, top_inst: TopologyInstance):
+        self.hosts = top_inst.get_hosts()
+        self.routers = top_inst.get_routers()
+        self.switches = top_inst.get_networks()
         self.links = []
         self.ports = []
 
-        self._remove_hidden_data_from_stack(sandbox, stack)
+        self.add_ports_and_links(top_inst)
+        self.remove_hidden_data(top_inst)
 
-        self.hosts = stack.hosts.values()
-        self.routers = stack.routers.values()
-        self.switches = stack.networks.values()
-
-        self._add_ports_and_links(stack)
-
-    def _add_ports_and_links(self, stack) -> None:
-        for ln in stack.links:
+    def add_ports_and_links(self, top_inst: TopologyInstance) -> None:
+        for ln in top_inst.links:
             real_port = self.Port(ln.ip, ln.mac, ln.node.name, ln.name + "_" + ln.node.name)
             dummy_port = self.Port(None, None, ln.network.name, ln.name + "_" + ln.network.name)
 
@@ -48,29 +39,19 @@ class Topology:
             self.ports.append(real_port)
             self.ports.append(dummy_port)
 
-    @staticmethod
-    def _remove_hidden_data_from_stack(sandbox: Sandbox, stack: Stack) -> None:
+    def remove_hidden_data(self, top_inst: TopologyInstance) -> None:
         """Removes data about management and hidden nodes from stack"""
-
         # Delete hidden host
-        definition = sandbox.allocation_unit.pool.definition
-        top_def = definitions.get_definition(definition.url, definition.rev, settings.KYPO_CONFIG)
-
-        hidden = []
-        for host in top_def.hosts:
-            if host.hidden:
-                hidden.append(host.name)
-                del stack.hosts[host.name]
+        self.hosts = [h for h in self.hosts if not h.hidden]
 
         # Delete MNG infrastructure
-        mng_nodes = (stack.man.name, stack.br.name, stack.uan.name)
-        mng_networks = (UAN_NET_NAME, BR_NET_NAME, stack.mng_net.name)
+        mng_nodes = [n.name for n in top_inst.get_extra_nodes()]
+        mng_networks = (UAN_NET_NAME, BR_NET_NAME, top_inst.man.name)
 
-        for net in mng_networks:
-            del stack.networks[net]
+        self.networks = [n for n in self.networks if n.name not in mng_networks]
 
         # Delete links
-        stack.links = [link for link in stack.links
-                       if link.node.name not in mng_nodes
-                       and link.node.name not in hidden
-                       and link.network.name not in mng_networks]
+        self.links = [link for link in self.links
+                      if link.node.name not in mng_nodes
+                      and not link.node.hidden
+                      and link.network.name not in mng_networks]
