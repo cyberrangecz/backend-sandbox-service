53dd5e204c85b05ab2a571cd69c0de5431099fc9
Miloslav Staněk <milostanek@gmail.com>
2020-05-05 14:33:17 +0200

    raise on timeout lock job

--- kypo/sandbox_instance_app/lib/jobs.py
+++ kypo/sandbox_instance_app/lib/jobs.py
@@ -7,23 +7,29 @@ from rq.job import Job
 LOG = structlog.get_logger()
 
 
-def lock_job(job: Job, timeout=60, step=5) -> None:
+def lock_job(job: Job, timeout: float = 60, step: float = 5) -> None:
+    """Lock given job; waits `timeout` seconds if not unlocked.
+
+    @raise TimeoutError if timeout is exceeded.
+    """
     stage = job.kwargs.get('stage')
     elapsed = 0
 
-    while elapsed <= timeout:
+    while elapsed < timeout:
         job.refresh()
         locked = job.meta.get('locked', True)
         if not locked:
             LOG.debug('Stage unlocked.', stage=stage)
-            break
+            return
         else:
             LOG.debug('Wait until the stage is unlocked.', stage=stage)
             time.sleep(step)
             elapsed += step
+    raise TimeoutError(f'Waiting for unlock of Job {job} exceeded timeout of {timeout} seconds.')
 
 
 def unlock_job(job: Job) -> None:
+    """Unlock given job."""
     stage = job.kwargs.get('stage')
     if job.meta.get('locked', True):
         LOG.debug('Unlocking stage.', stage=stage)
@@ -32,6 +38,7 @@ def unlock_job(job: Job) -> None:
 
 
 def delete_job(job_id: str) -> None:
+    """Delete job, with given id."""
     try:
         Job.fetch(job_id, connection=Redis()).delete(delete_dependents=True)
     except NoSuchJobError:  # Job already deleted
