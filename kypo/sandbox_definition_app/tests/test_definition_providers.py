510a1eaaadc50f0708bfe4aa2486044322cbcbd7
Michal Urban <485429@mail.muni.cz>
2021-08-19 13:57:00 +0200

    Resolve "Refactor tests in definition app"

--- kypo/sandbox_definition_app/tests/test_definition_providers.py
+++ kypo/sandbox_definition_app/tests/test_definition_providers.py
@@ -1,8 +1,15 @@
 import pytest
+import requests
+
+from gitlab import GitlabError
 
 from kypo.sandbox_common_lib.exceptions import GitError
 from kypo.sandbox_common_lib.kypo_config import KypoConfiguration
 from kypo.sandbox_definition_app.lib.definition_providers import GitlabProvider, InternalGitProvider
+
+
+EXPECTED_RESULT_ARRAY = ['t', 'e', 's', 't']
+EXPECTED_RESULT_STR = "test"
 
 
 class TestGitlabProvider:
@@ -17,32 +24,241 @@ class TestGitlabProvider:
 
     @pytest.mark.parametrize('url', [URL1, URL2, URL3])
     def test_get_project_path(self, url):
-        provider1 = GitlabProvider(url, self.CFG)
+        gitlab_provider = GitlabProvider(url, self.CFG)
+        assert gitlab_provider.project_path == self.get_expected_url(url)
 
-        assert provider1.project_path == self.get_expected_url(url)
+    @pytest.fixture
+    def gitlab_project(self, mocker):
+        project = mocker.MagicMock()
+        return project
+
+    @pytest.fixture
+    def gitlab_provider(self, mocker, gitlab_project):
+        gitlab_provider = GitlabProvider(self.URL1, self.CFG)
+        gitlab_provider.gl.projects.get = mocker.MagicMock()
+        gitlab_provider.gl.projects.get.return_value = gitlab_project
+        return gitlab_provider
+
+    def test_get_file(self, mocker, gitlab_provider, gitlab_project):
+        file = mocker.MagicMock()
+        file.decode.return_value = EXPECTED_RESULT_STR.encode()
+        gitlab_project.files.get.return_value = file
+        assert gitlab_provider.get_file('path', 'rev') == EXPECTED_RESULT_STR
+
+    def test_get_file_project_not_found(self, gitlab_provider):
+        gitlab_provider.gl.projects.get.side_effect \
+            = GitlabError('project request error')
+        with pytest.raises(GitError):
+            gitlab_provider.get_file('path', 'rev')
+
+    def test_get_file_file_not_found(self, gitlab_provider, gitlab_project):
+        gitlab_project.files.get.side_effect \
+            = requests.exceptions.RequestException('file request error')
+        with pytest.raises(GitError):
+            gitlab_provider.get_file('path', 'rev')
+
+    def test_get_branches(self, gitlab_provider, gitlab_project):
+        gitlab_project.branches.list.return_value = EXPECTED_RESULT_ARRAY
+        assert gitlab_provider.get_branches() == EXPECTED_RESULT_ARRAY
+
+    def test_get_branches_project_not_found(self, gitlab_provider):
+        gitlab_provider.gl.projects.get.side_effect = GitlabError('project request error')
+        with pytest.raises(GitError):
+            gitlab_provider.get_branches()
+
+    def test_get_branches_branches_not_found(self, gitlab_provider, gitlab_project):
+        gitlab_project.branches.list.side_effect \
+            = requests.exceptions.RequestException('branch request mock error')
+        with pytest.raises(GitError):
+            gitlab_provider.get_branches()
+
+    def test_get_tags(self, gitlab_provider, gitlab_project):
+        gitlab_project.tags.list.return_value = EXPECTED_RESULT_ARRAY
+        assert gitlab_provider.get_tags() == EXPECTED_RESULT_ARRAY
+
+    def test_get_tags_project_not_found(self, gitlab_provider):
+        gitlab_provider.gl.projects.get.side_effect = GitlabError('project request mock error')
+        with pytest.raises(GitError):
+            gitlab_provider.get_tags()
+
+    def test_get_tags_tags_not_found(self, gitlab_provider, gitlab_project):
+        gitlab_project.tags.list.side_effect \
+            = requests.exceptions.RequestException('tags request mock error')
+        with pytest.raises(GitError):
+            gitlab_provider.get_tags()
+
+    def test_get_refs(self, mocker, gitlab_provider):
+        gitlab_provider.get_branches = mocker.MagicMock()
+        gitlab_provider.get_branches.return_value = EXPECTED_RESULT_ARRAY[:2]
+        gitlab_provider.get_tags = mocker.MagicMock()
+        gitlab_provider.get_tags.return_value = EXPECTED_RESULT_ARRAY[2:]
+        assert gitlab_provider.get_refs() == EXPECTED_RESULT_ARRAY
+
+    def test_get_rev_sha_from_refs(self, mocker, gitlab_provider):
+        revision1 = mocker.MagicMock()
+        revision1.name = "other"
+        revision1.id = "1"
+
+        rev_name = "test"
+        expected_rev_sha = "2"
+        revision2 = mocker.MagicMock()
+        revision2.name = rev_name
+        revision2.id = expected_rev_sha
+        revision2.commit = {'id': expected_rev_sha}
+
+        gitlab_provider.get_refs = mocker.MagicMock()
+        gitlab_provider.get_refs.return_value = [revision1, revision2]
+        assert gitlab_provider.get_rev_sha(rev_name) == expected_rev_sha
+
+    def test_get_rev_sha_from_commits(self, mocker, gitlab_provider, gitlab_project):
+        expected_id = "1"
+        commit = mocker.MagicMock()
+        commit.id = expected_id
+        gitlab_project.commits.get.return_value = commit
+
+        gitlab_provider.get_refs = mocker.MagicMock()
+        gitlab_provider.get_refs.return_value = []
+        assert gitlab_provider.get_rev_sha("rev") == expected_id
+
+    def test_get_rev_sha_project_not_found(self, gitlab_provider):
+        gitlab_provider.gl.projects.get.side_effect \
+            = GitlabError('project request mock error')
+        with pytest.raises(GitError):
+            gitlab_provider.get_rev_sha("rev")
+
+    def test_get_rev_sha_commit_not_found(self, gitlab_provider, gitlab_project):
+        gitlab_project.commits.get.side_effect \
+            = requests.exceptions.RequestException('commit request mock error')
+        with pytest.raises(GitError):
+            gitlab_provider.get_rev_sha("rev")
 
 
 class TestInternalGitProvider:
-    URL_valid_1 = 'git@localhost.lan:/repos/nested-folder/myrepo.git'
-    URL_valid_2 = 'ssh://git@localhost.lan/repos/myrepo.git'
-    URL_invalid = 'git@localhost.lan:repositories/nested-folder/next-folder/myrepo.git'
-
+    URL_VALID_1 = 'git@localhost.lan:/repos/nested-folder/myrepo.git'
+    URL_VALID_1_REST = 'http://localhost.lan:8081/repos/nested-folder;myrepo.git'
+    URL_VALID_2 = 'ssh://git@localhost.lan/repos/myrepo.git'
+    URL_VALID_2_REST = 'http://localhost.lan:8081/repos/myrepo.git'
+    URL_INVALID = 'git@localhost.lan:repositories/nested-folder/next-folder/myrepo.git'
     CFG = KypoConfiguration(git_server='localhost.lan', git_rest_server='http://localhost.lan:8081')
 
-    def test_get_repo_url(self):
-        provider1 = InternalGitProvider(self.URL_valid_1, self.CFG)
-        assert provider1.rest_url == 'http://localhost.lan:8081/repos/nested-folder;myrepo.git'
-
-        provider2 = InternalGitProvider(self.URL_valid_2, self.CFG)
-        assert provider2.rest_url == 'http://localhost.lan:8081/repos/myrepo.git'
+    @pytest.mark.parametrize('valid_url, expected_rest_url', [(URL_VALID_1, URL_VALID_1_REST),
+                                                              (URL_VALID_2, URL_VALID_2_REST)])
+    def test_get_repo_url(self, valid_url, expected_rest_url):
+        provider = InternalGitProvider(valid_url, self.CFG)
+        assert provider.rest_url == expected_rest_url
 
     def test_invalid_path(self):
         with pytest.raises(GitError):
-            InternalGitProvider(self.URL_invalid, self.CFG)
+            InternalGitProvider(self.URL_INVALID, self.CFG)
+
+    @pytest.fixture
+    def internal_provider(self):
+        return InternalGitProvider(self.URL_VALID_1, self.CFG)
+
+    @pytest.fixture
+    def internal_provider_request(self, mocker, internal_provider):
+        internal_provider.get_request = mocker.MagicMock()
+        return internal_provider
+
+    @pytest.mark.parametrize('valid_url', [URL_VALID_1, URL_VALID_2])
+    def test_validate(self, internal_provider, valid_url):
+        url_parsed = internal_provider.validate(valid_url, self.CFG)
+        assert url_parsed.pathname == '/repos' + valid_url.split('/repos')[1]
+        assert url_parsed.protocol == 'ssh'
+        assert url_parsed.href == valid_url
+
+    @pytest.fixture
+    def response_success(self, mocker):
+        response_success = mocker.MagicMock()
+        response_success.json.return_value = EXPECTED_RESULT_STR
+        return response_success
+
+    def test_get_file(self, internal_provider_request, response_success):
+        response_success.text = EXPECTED_RESULT_STR
+        internal_provider_request.get_request.return_value = response_success
+        assert internal_provider_request.get_file('path', 'rev') == EXPECTED_RESULT_STR
+        internal_provider_request.get_request.\
+            assert_called_with(f'{self.URL_VALID_1_REST}/raw/rev/path')
+
+    def test_get_file_connection_error(self, internal_provider):
+        with pytest.raises(GitError):
+            internal_provider.get_file('path', 'rev')
+
+    def test_get_file_request_error(self, internal_provider_request):
+        internal_provider_request.get_request.side_effect =\
+            requests.RequestException("test exception")
+        with pytest.raises(GitError):
+            internal_provider_request.get_file('path', 'rev')
+
+    def test_get_branches(self, internal_provider_request, response_success):
+        internal_provider_request.get_request.return_value = response_success
+        assert internal_provider_request.get_branches() == EXPECTED_RESULT_STR
+        internal_provider_request.get_request.\
+            assert_called_with(f'{self.URL_VALID_1_REST}/branches/')
+
+    def test_get_branches_connection_error(self, internal_provider):
+        with pytest.raises(GitError):
+            internal_provider.get_branches()
+
+    def test_get_branches_request_error(self, internal_provider_request):
+        internal_provider_request.get_request.side_effect =\
+            requests.RequestException("test exception")
+        with pytest.raises(GitError):
+            internal_provider_request.get_branches()
+
+    def test_get_tags(self, internal_provider_request, response_success):
+        internal_provider_request.get_request.return_value = response_success
+        assert internal_provider_request.get_tags() == EXPECTED_RESULT_STR
+        internal_provider_request.get_request.assert_called_with(f'{self.URL_VALID_1_REST}/tags/')
+
+    def test_get_tags_connection_error(self, internal_provider):
+        with pytest.raises(GitError):
+            internal_provider.get_tags()
+
+    def test_get_tags_request_error(self, internal_provider_request):
+        internal_provider_request.get_request.side_effect =\
+            requests.RequestException("test exception")
+        with pytest.raises(GitError):
+            internal_provider_request.get_tags()
+
+    def test_get_refs(self, mocker, internal_provider):
+        internal_provider.get_branches = mocker.MagicMock()
+        internal_provider.get_branches.return_value = EXPECTED_RESULT_ARRAY[:2]
+        internal_provider.get_tags = mocker.MagicMock()
+        internal_provider.get_tags.return_value = EXPECTED_RESULT_ARRAY[2:]
+        assert internal_provider.get_refs() == EXPECTED_RESULT_ARRAY
+
+    def test_get_rev_sha(self, internal_provider_request, response_success):
+        expected_result = {'sha': '1'}
+        response_success.json.return_value = expected_result
+
+        internal_provider_request.get_request.return_value = response_success
+        assert internal_provider_request.get_rev_sha('rev') == expected_result['sha']
+        internal_provider_request.get_request.\
+            assert_called_with(f'{self.URL_VALID_1_REST}/commits/rev')
+
+    def test_get_rev_sha_connection_error(self, internal_provider):
+        with pytest.raises(GitError):
+            internal_provider.get_rev_sha('rev')
+
+    def test_get_request(self, mocker, internal_provider):
+        expected_value = requests.Response()
+        expected_value.status_code = 202
+        requests.get = mocker.MagicMock()
+        requests.get.return_value = expected_value
+        assert internal_provider.get_request('url') == expected_value
+        requests.get.assert_called_with('url')
+
+    def test_get_request_raises_exception(self, mocker, internal_provider):
+        expected_value = mocker.MagicMock()
+        expected_value.raise_for_status.side_effect = requests.HTTPError("test exception")
+        requests.get = mocker.MagicMock()
+        requests.get.return_value = expected_value
+        with pytest.raises(requests.HTTPError):
+            internal_provider.get_request('url')
 
 
 @pytest.mark.integration
 class TestGitIntegration:
-
     def test_def_provider(self):
         assert True
--- kypo/sandbox_definition_app/tests/test_definitions.py
+++ kypo/sandbox_definition_app/tests/test_definitions.py
@@ -2,63 +2,67 @@ import pytest
 from rest_framework.exceptions import ValidationError as RestValidationError
 from yamlize import YamlizingError
 
-from kypo.sandbox_common_lib.exceptions import ValidationError
+
+from kypo.sandbox_common_lib.kypo_config import KypoConfiguration, GitType
 from kypo.sandbox_definition_app.lib import definitions
+from kypo.sandbox_definition_app.lib.definition_providers import InternalGitProvider, GitlabProvider
 from kypo.sandbox_definition_app.models import Definition
+from kypo.sandbox_common_lib import exceptions
 
 pytestmark = pytest.mark.django_db
 
 
 class TestCreateDefinition:
-    url = 'def-url'
-    rev = 'def-rev'
-    name = 'def-name'
+    URL = 'def-url'
+    REV = 'def-rev'
+    NAME = 'def-name'
 
-    def test_create_definition_success(self, mocker):
-        mocker.patch("kypo.sandbox_definition_app.lib.definitions.DefinitionProvider")
-        mocker.patch("kypo.sandbox_definition_app.lib.definitions.validate_topology_definition")
-        mocker.patch("kypo.openstack_driver.KypoOpenStackClient.validate_topology_definition")
-        mock = mocker.Mock()
-        mock.configure_mock(name='def-name')
-        mocker.patch("kypo.sandbox_definition_app.lib.definitions.get_definition",
-                     return_value=mock)
-
-        definitions.create_definition(url=self.url, rev=self.rev)
-
-        # Definition name is tested by get
-        database_definition = Definition.objects.get(name=self.name)
-        assert database_definition.url == self.url
-        assert database_definition.rev == self.rev
-
-    def test_create_definition_nonunique(self, mocker):
-        mocker.patch("kypo.sandbox_definition_app.lib.definitions.DefinitionProvider")
-        mocker.patch("kypo.sandbox_definition_app.lib.definitions.validate_topology_definition")
-        mocker.patch("kypo.openstack_driver.KypoOpenStackClient.validate_topology_definition")
-        mock = mocker.Mock()
-        mock.configure_mock(name='def-name')
-        mocker.patch("kypo.sandbox_definition_app.lib.definitions.get_definition",
-                     return_value=mock)
-
-        definitions.create_definition(url=self.url, rev=self.rev)
-        with pytest.raises(RestValidationError):
-            definitions.create_definition(url=self.url, rev=self.rev)
-
-    def test_create_definition_invalid_hosts_group(self, mocker):
+    @pytest.fixture(autouse=True)
+    def setup(self, mocker):
         mocker.patch("kypo.sandbox_definition_app.lib.definitions.DefinitionProvider")
         mocker.patch("kypo.openstack_driver.KypoOpenStackClient.validate_topology_definition")
-        hosts_group = mocker.Mock()
-        hosts_group.name = 'hidden_hosts'
+
+    @pytest.fixture
+    def topology_definition(self, mocker):
         topology_definition = mocker.Mock()
-        topology_definition.configure_mock(name='def-name', groups=[hosts_group])
+        topology_definition.name = self.NAME
         mocker.patch("kypo.sandbox_definition_app.lib.definitions.get_definition",
                      return_value=topology_definition)
+        return topology_definition
 
-        with pytest.raises(ValidationError):
-            definitions.create_definition(url=self.url, rev=self.rev)
+    def test_create_definition(self, mocker, topology_definition):
+        mocker.patch("kypo.sandbox_definition_app.lib.definitions.validate_topology_definition")
+        definitions.create_definition(url=self.URL, rev=self.REV)
+
+        # Definition name is tested by get
+        database_definition = Definition.objects.get(name=self.NAME)
+        assert database_definition.url == self.URL
+        assert database_definition.rev == self.REV
+
+    def test_create_definition_nonunique(self, mocker, topology_definition):
+        mocker.patch("kypo.sandbox_definition_app.lib.definitions.validate_topology_definition")
+        definitions.create_definition(url=self.URL, rev=self.REV)
+        with pytest.raises(RestValidationError):
+            definitions.create_definition(url=self.URL, rev=self.REV)
+
+    def test_create_definition_invalid_hosts_group(self, mocker, topology_definition):
+        hosts_group = mocker.Mock()
+        hosts_group.name = 'hidden_hosts'
+        topology_definition.groups = [hosts_group]
+        with pytest.raises(exceptions.ValidationError):
+            definitions.create_definition(url=self.URL, rev=self.REV)
+
+    def test_create_definition_inaccessible_by_ssh(self, mocker, topology_definition):
+        mocker.patch("kypo.sandbox_definition_app.lib.definitions.validate_topology_definition")
+        mocker.patch("kypo.sandbox_definition_app.lib.definitions.DefinitionProvider"
+                     ".has_key_access", return_value=False)
+
+        with pytest.raises(exceptions.ValidationError):
+            definitions.create_definition(url=self.URL, rev=self.REV)
 
 
 class TestLoadDefinition:
-    def test_load_definition_success(self, topology_definition_stream):
+    def test_load_definition(self, topology_definition_stream):
         topology_definition = definitions.load_definition(topology_definition_stream)
 
         for host in topology_definition.hosts:
@@ -74,5 +78,55 @@ class TestLoadDefinition:
             .patch("kypo.sandbox_definition_app.lib.definitions.TopologyDefinition")
         topology_definition.load.side_effect = YamlizingError('exception-text')
 
-        with pytest.raises(ValidationError):
+        with pytest.raises(exceptions.ValidationError):
             definitions.load_definition(topology_definition_stream)
+
+
+class TestGetDefinition:
+    CFG = KypoConfiguration(git_server='localhost.lan',
+                            git_rest_server='http://localhost.lan:8081')
+
+    def test_get_definition(self, mocker):
+        topology_provider = mocker.MagicMock()
+        topology_provider.get_file.return_value = "test1"
+        mocker.patch("kypo.sandbox_definition_app.lib.definitions.get_def_provider",
+                     return_value=topology_provider)
+        mocker.patch("kypo.sandbox_definition_app.lib.definitions.io.StringIO",
+                     return_value="test2")
+        mocker.patch("kypo.sandbox_definition_app.lib.definitions.load_definition",
+                     return_value="test3")
+        assert definitions.get_definition('url', 'rev', self.CFG) == "test3"
+        definitions.load_definition.assert_called_with(definitions.io.StringIO("test1"))
+
+    def test_get_definition_file_not_found(self, mocker):
+        topology_provider = mocker.MagicMock()
+        topology_provider.get_file.side_effect = exceptions.GitError("file not found error")
+        mocker.patch("kypo.sandbox_definition_app.lib.definitions.get_def_provider",
+                     return_value=topology_provider)
+        with pytest.raises(exceptions.GitError):
+            definitions.get_definition('url', 'rev', self.CFG)
+
+
+class TestGetDefProvider:
+    def test_get_def_provider_internal(self):
+        url_internal = 'git@localhost.lan:/repos/nested-folder/myrepo.git'
+        cfg_internal = KypoConfiguration(git_server='localhost.lan',
+                                    git_rest_server='http://localhost.lan:8081',
+                                    git_type=GitType.INTERNAL)
+        assert isinstance(definitions.get_def_provider(url_internal, cfg_internal),
+                          InternalGitProvider)
+
+    def test_get_def_provider_gitlab(self):
+        url_git = 'git@gitlab.com:kypo-crp/backend-python/kypo-sandbox-service.git'
+        cfg_git = KypoConfiguration(git_server='gitlab.com',
+                                    git_rest_server='http://gitlab.com:8081',
+                                    git_type=GitType.GITLAB)
+        assert isinstance(definitions.get_def_provider(url_git, cfg_git),
+                          GitlabProvider)
+
+    def test_get_def_improperly_configured(self, mocker):
+        url_git = 'git@gitlab.com:kypo-crp/backend-python/kypo-sandbox-service.git'
+        cfg_git = mocker.MagicMock()
+        cfg_git.git_type = 3
+        with pytest.raises(exceptions.ImproperlyConfigured):
+            definitions.get_def_provider(url_git, cfg_git)
