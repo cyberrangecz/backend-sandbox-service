cb04738a2526c10b5c3e21bc5bd8b2329e1b73c8
Michal Urban <485429@mail.muni.cz>
2022-10-07 11:57:07 +0200

    updated kubernetes container volume mounts to enable containers in sandbox definitions

--- kypo/sandbox_ansible_app/lib/container.py
+++ kypo/sandbox_ansible_app/lib/container.py
@@ -200,6 +200,12 @@ class KubernetesContainer(BaseContainer):
                 mount_path=self.ANSIBLE_INVENTORY_PATH.bind,
                 sub_path=self.inventory_path[
                          len(settings.KYPO_CONFIG.ansible_runner_settings.volumes_path)+1:]
+            ),
+            client.V1VolumeMount(
+                name=ANSIBLE_FILE_VOLUME_NAME,
+                mount_path=self.ANSIBLE_DOCKER_CONTAINER_PATH.bind,
+                sub_path=self.inventory_path[
+                         len(settings.KYPO_CONFIG.ansible_runner_settings.volumes_path) + 1:]
             )
         ]
 
