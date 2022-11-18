c87ea84f6614ff3876055261ee1588ba8b4dbda3
Michal Urban <485429@mail.muni.cz>
2022-11-18 14:33:53 +0100

    added kypo global sandbox id variable to inventory file

--- kypo/sandbox_ansible_app/lib/ansible.py
+++ kypo/sandbox_ansible_app/lib/ansible.py
@@ -219,6 +219,7 @@ class AllocationAnsibleRunner(AnsibleRunner):
         terraformstack = sas.terraformstack
         extra_vars = {
             'kypo_global_sandbox_allocation_unit_id': sau.id,
+            'kypo_global_sandbox_id': sau.sandbox.id,
             'kypo_global_openstack_stack_id': terraformstack.stack_id,
             'kypo_global_pool_id': sau.pool.id,
             'kypo_global_head_ip': settings.KYPO_CONFIG.kypo_head_ip,
@@ -240,6 +241,7 @@ class CleanupAnsibleRunner(AnsibleRunner):
                                          allocation_unit.get_stack_name())
         inventory_object.add_variables(
             kypo_global_sandbox_allocation_unit_id=allocation_unit.id,
+            kypo_global_sandbox_id=allocation_unit.sandbox.id,
             kypo_global_pool_id=allocation_unit.pool.id
         )
 
